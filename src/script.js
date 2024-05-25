2dbb3c63ed374e702a8360d013212e9c1ac5c70b
kongnoun <kongnoun00@gmail.com>
2024-05-25 17:08:20 +0700

    changes toward navbar and slideshows

---
+++ src/script.js
@@ -0,0 +1,42 @@
+import '../style/modern-normalize.css';
+import '../style/style.css';
+import '../style/utils.css';
+
+// document.addEventListener('contextmenu', event => event.preventDefault());
+
+// function hideSidebar() {
+//     var sidebar = document.querySelector('.sidebar');
+//     sidebar.style.display = 'none';
+//     sidebar.classList.remove('sidebar--visible');
+
+//         }
+
+// function showSidebar() {
+//     var sidebar = document.querySelector('.sidebar');
+//     sidebar.style.display = 'flex';
+//     sidebar.classList.add('sidebar--visible');
+
+
+// }
+
+
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// function changeSlide(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// function showSlides(n) {
+//     const slides = document.getElementsByClassName("mySlides");
+//     if (n > slides.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length;
+//     }
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+// }
